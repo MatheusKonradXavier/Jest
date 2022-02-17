f1422e31b0dc41b2b7f7325454f5a2b2a42bf3a5
Konrad <matheuskonradxavier@hotmail.com>
2022-02-17 15:45:09 -0300

    Primeiro teste com Jest

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO SHOW', () => {
+  it('descrição do teste(IT)', () => {
+    const number = 1;
+    expect(number).not.toBe(1);
+  });
+});
+
+describe('TESTANDO SHOW', () => {
+  test('descrição de teste (TESTE)', () => {
+    const nome = 'konrad';
+    expect(nome).not.toBe('konrad');
+  });
+});
